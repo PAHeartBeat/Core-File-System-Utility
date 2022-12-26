da62ef139cf7f327f21e2237a748fcf85e613112
iPAHeartBeat <ankur30884@gmail.com>
2022-12-27 00:48:22 +0530

    Minor Fixes done for export code and remove extra dependencies

--- CreatePacakges.bat
+++ CreatePacakges.bat
@@ -9,13 +9,18 @@ dotnet pack Src/. --include-symbols --force -c Release --output ./Packages/NuGet
 @REM Creating Unity NPM Project
 ECHO Step 1: Publishing Code for Unity Package
 set location=com.iPAHeartBeat.Core.FileSystemUtility
-dotnet publish Src/%location%.csproj -c Release --no-dependencies --framework net471 --output ./Unity/Packages/%location%/Runtime/.
+dotnet publish Src/. -c Release --no-dependencies --framework net471 --output ./Unity/Packages/%location%/Runtime/.
 
 @REM Removing Extra DLL for Which Code Will avaialble via Unity package registries.
 ECHO Step 2: Removing Extra DLLs
 cd Unity/Packages/%location%/Runtime
 del Newtonsoft.Json.dll
-del iPAHeartBeat.Core.FileSystemUtility.dll
+del iPAHeartBeat.Core.Abstraction.dll
+del iPAHeartBeat.Core.Extensions.dll
+del iPAHeartBeat.Core.Dependency.dll
+del iPAHeartBeat.Core.Extensions-net471.dll
+del iPAHeartBeat.Core.Dependency-net471.dll
+rename iPAHeartBeat.Core.FileSystemUtility-net471.xml iPAHeartBeat.Core.FileSystemUtility.xml
 cd..
 cd..
 cd..
