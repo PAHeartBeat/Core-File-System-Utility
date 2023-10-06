e996ca325e6b0659edbee2e9c09e788a5e7e7753
iPAHeartBeat <ankur30884@gmail.com>
2023-10-06 23:44:19 +0530

    Mac Batch file updated.

--- CreatePacakgesMac.bat
+++ CreatePacakgesMac.bat
@@ -16,10 +16,13 @@ dotnet publish Src/$LOCATION.csproj -c Release --no-dependencies --framework net
 
 @REM Removing Extra DLL for Which Code Will available via Unity package registries.
 ECHO Step 2: Removing Extra DLLs
-rm -f ./Unity/Packages/%LOCATION%/Runtime/Newtonsoft.Json.dll
-rm -f ./Unity/Packages/%LOCATION%/Runtime/iPAHeartBeat.Core.Abstraction.dll
-rm -f ./Unity/Packages/%LOCATION%/Runtime/iPAHeartBeat.Core.Extensions.dll
-rm -f ./Unity/Packages/%LOCATION%/Runtime/iPAHeartBeat.Core.Dependency.dll
+rm ./Unity/Packages/$LOCATION/Runtime/Newtonsoft.Json.dll -f
+rm ./Unity/Packages/$LOCATION/Runtime/iPAHeartBeat.Core.Abstraction.dll -f
+rm ./Unity/Packages/$LOCATION/Runtime/iPAHeartBeat.Core.Extensions.dll -f
+rm ./Unity/Packages/$LOCATION/Runtime/iPAHeartBeat.Core.Dependency.dll -f
+rm ./Unity/Packages/$LOCATION/Runtime/iPAHeartBeat.Core.Abstraction.pdb -f
+rm ./Unity/Packages/$LOCATION/Runtime/iPAHeartBeat.Core.Extensions.pdb -f
+rm ./Unity/Packages/$LOCATION/Runtime/iPAHeartBeat.Core.Dependency.pdb -f
 
 @REM Create Node package for Unity
 ECHO Step 3: Creating Node Package for Unity Package Manager.
