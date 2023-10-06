454569611cfe156599c1124eb5dce2baf79b5629
iPAHeartBeat <ankur30884@gmail.com>
2023-10-06 23:30:10 +0530

    Windows bat file updated. Mac Batch file created.

--- CreatePacakgesMac.bat
+++ CreatePacakgesMac.bat
@@ -10,20 +10,20 @@ dotnet pack Src/. --include-symbols --force -c Release --output ./Packages/NuGet
 
 @REM Creating Unity NPM Project
 ECHO Step 1: Publishing Code for Unity Package
-set LOCATION=com.iPAHeartBeat.Core.FileSystemUtility
-del ./Unity/Packages/%LOCATION%/Runtime/*.*
-dotnet publish Src/%LOCATION%.csproj -c Release --no-dependencies --framework net471 --output ./Unity/Packages/%LOCATION%/Runtime/.
+export LOCATION=com.iPAHeartBeat.Core.FileSystemUtility
+rm Unity/Packages/$LOCATION/Runtime/* -r
+dotnet publish Src/$LOCATION.csproj -c Release --no-dependencies --framework net471 --output ./Unity/Packages/$LOCATION/Runtime/.
 
 @REM Removing Extra DLL for Which Code Will available via Unity package registries.
 ECHO Step 2: Removing Extra DLLs
-del ./Unity/Packages/%LOCATION%/Runtime/Newtonsoft.Json.dll
-del ./Unity/Packages/%LOCATION%/Runtime/iPAHeartBeat.Core.Abstraction.dll
-del ./Unity/Packages/%LOCATION%/Runtime/iPAHeartBeat.Core.Extensions.dll
-del ./Unity/Packages/%LOCATION%/Runtime/iPAHeartBeat.Core.Dependency.dll
+rm -f ./Unity/Packages/%LOCATION%/Runtime/Newtonsoft.Json.dll
+rm -f ./Unity/Packages/%LOCATION%/Runtime/iPAHeartBeat.Core.Abstraction.dll
+rm -f ./Unity/Packages/%LOCATION%/Runtime/iPAHeartBeat.Core.Extensions.dll
+rm -f ./Unity/Packages/%LOCATION%/Runtime/iPAHeartBeat.Core.Dependency.dll
 
 @REM Create Node package for Unity
 ECHO Step 3: Creating Node Package for Unity Package Manager.
-cd Unity/Packages/%LOCATION%
+cd Unity/Packages/$LOCATION
 npm pack --pack-destination="../../../Packages/UnityNPM/"
 cd ..
 cd ..
